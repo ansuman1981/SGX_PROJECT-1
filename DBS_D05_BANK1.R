ced594a7739d14183c01754109074b7c449a33c7
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-04-09 19:36:28 +0800

    check and arrange the data

--- DBS_D05_BANK1.R
+++ DBS_D05_BANK1.R
@@ -1,5 +1,11 @@
 install.packages("readr")
 library(readr)
+library(dplyr)
 D05 <- read.csv("D05.SI_2024-2026.csv")
 names(D05)
 summary(D05)
+dbs <- D05 |>  # arrange check
+  arrange(date)
+
+sum(is.na(dbs$close))
+sum(dbs$close == 0, na.rm = TRUE)
