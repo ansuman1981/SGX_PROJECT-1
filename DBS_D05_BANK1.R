4c2eee6937b186107d399430e3b61e07a047ecc3
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-04-10 10:43:08 +0800

    taking data for 2024  to 2025 only and filte it

--- DBS_D05_BANK1.R
+++ DBS_D05_BANK1.R
@@ -1,11 +1,26 @@
 install.packages("readr")
 library(readr)
 library(dplyr)
+library(lubridate)
 D05 <- read.csv("D05.SI_2024-2026.csv")
 names(D05)
-summary(D05)
-dbs <- D05 |>  # arrange check
-  arrange(date)
+dbs <- D05
+sum(is.na(dbs$close)) # check any NA values 
+sum(dbs$close == 0, na.rm = TRUE) # if both return 0 fit to go 
 
-sum(is.na(dbs$close))
-sum(dbs$close == 0, na.rm = TRUE)
+### craete a sort data for year 2024-2025
+
+# Tells R: "My date is Day-Month-Year"
+dbs$date <- as.Date(dbs$date, format = "%d-%m-%Y")
+
+# Now check the first few rows again
+head(dbs$date)
+
+# filter the whole date 
+clean_date <- dbs %>%
+  filter(date >= "2024-01-01" & date <= "2025-12-31")
+class(clean_date$date)
+nrow(clean_date)
+head(dbs$date)
+
+
